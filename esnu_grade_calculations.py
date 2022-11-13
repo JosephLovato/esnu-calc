b1289deb07ebf1b31de7d82e392ea8e817fdc5c1
JosephLovato <jlovato@mymail.mines.edu>
2022-11-13 16:53:44 -0700

    updated unit tests for programming score with U, added some string score converters

--- esnu_grade_calculations.py
+++ esnu_grade_calculations.py
@@ -1,10 +1,11 @@
 #
 # ESNU Grading Calculations Helper Functions
 # author: Joey Lovato
-# last updated: 10/13/22
+# last updated: 11/13/22
 #
 
 from enum import Enum
+
 
 class Score(Enum):
     E = 4
@@ -12,14 +13,45 @@ class Score(Enum):
     N = 2
     U = 1
     X = 0
+
     def __ge__(self, other):
         return self.value >= other.value
+
     def __le__(self, other):
         return self.value <= other.value
+
     def __gt__(self, other):
         return self.value > other.value
+
     def __lt__(self, other):
         return self.value < other.value
+
+
+def convert_to_score(s: str):
+    if s == "E":
+        return Score.E
+    elif s == "S":
+        return Score.S
+    elif s == "N":
+        return Score.N
+    elif s == "U":
+        return Score.U
+    else:
+        raise RuntimeError("Cannot convert score: " + s)
+
+
+def score_to_string(score):
+    if score == Score.E:
+        return "E"
+    elif score == Score.S:
+        return "S"
+    elif score == Score.N:
+        return "N"
+    elif score == Score.U:
+        return "U"
+    else:
+        raise RuntimeError("Cannot convert score: " + score)
+
 
 def count_at_least(scores: list, minimum: Score):
     """returns the number of scores greater than
@@ -27,9 +59,10 @@ def count_at_least(scores: list, minimum: Score):
     """
     return [x.value >= minimum.value for x in scores].count(True)
 
+
 def compute_comprehension_score(final, midterm):
     """Computes the ESNU comprehensions component score given the final and midterm scores"""
-    if final == Score.E: 
+    if final == Score.E:
         return Score.E
     elif final == Score.S or (midterm == Score.E and final == Score.N):
         return Score.S
@@ -39,6 +72,7 @@ def compute_comprehension_score(final, midterm):
         return Score.U
     else:
         return Score.X
+
 
 def compute_programming_score(scores: list):
     """Computes the ESNU programming component score given a list of programming scores"""
@@ -53,6 +87,7 @@ def compute_programming_score(scores: list):
     else:
         return Score.X
 
+
 def compute_analysis_score(scores: list):
     """Computes the ESNU analysis component score given a list of analysis scores"""
     if count_at_least(scores, Score.S) >= 4 and scores.count(Score.E) >= 2:
@@ -65,6 +100,7 @@ def compute_analysis_score(scores: list):
         return Score.U
     else:
         return Score.X
+
 
 def compute_final_score(component_scores: list):
     """Computes final A-F grade given a list of three component scores
@@ -80,8 +116,8 @@ def compute_final_score(component_scores: list):
     elif component_scores.count(Score.S) <= 1 or component_scores.count(Score.U) > 0:
         return 'F'
 
-def calculate_grade(programming_scores, analysis_scores, midterm_score, final_score):
-    return compute_final_score([compute_programming_score(programming_scores), 
-                                compute_analysis_score(analysis_scores), 
-                                compute_comprehension_score(final_score, midterm_score)])
 
+def calculate_grade(programming_scores, analysis_scores, midterm_score, final_score):
+    return compute_final_score([compute_programming_score(programming_scores),
+                                compute_analysis_score(analysis_scores),
+                                compute_comprehension_score(final_score, midterm_score)])
--- unit_tests.py
+++ unit_tests.py
@@ -11,108 +11,193 @@ lga_q_5 = {"programming": [Score.S] * 4 + [Score.U],
            "midterm": Score.N,  # doesn't matter
            "final": Score.S}
 
+
 class TestProgrammingCalculation(unittest.TestCase):
     def test_lga_q_4(self):
-        self.assertEqual(Score.E, compute_programming_score(lga_q_4["programming"]))
+        self.assertEqual(Score.E, compute_programming_score(
+            lga_q_4["programming"]))
+
     def test_lga_q_5(self):
-        self.assertEqual(Score.S, compute_programming_score(lga_q_5["programming"]))
+        self.assertEqual(Score.S, compute_programming_score(
+            lga_q_5["programming"]))
+
     def test_all_possible_E(self):
         self.assertEqual(Score.E, compute_programming_score([Score.S] * 5))
+
     def test_all_possible_S(self):
-        self.assertEqual(Score.S, compute_programming_score([Score.S] * 4 + [Score.N]))
+        self.assertEqual(Score.S, compute_programming_score(
+            [Score.S] * 4 + [Score.N]))
+        self.assertEqual(Score.S, compute_programming_score(
+            [Score.S] * 4 + [Score.U]))
+
     def test_all_possible_N(self):
-        self.assertEqual(Score.N, compute_programming_score([Score.S] * 3 + [Score.N] * 2))
-        self.assertEqual(Score.N, compute_programming_score([Score.S] * 2 + [Score.N] * 3))
+        self.assertEqual(Score.N, compute_programming_score(
+            [Score.S] * 3 + [Score.N] * 2))
+        self.assertEqual(Score.N, compute_programming_score(
+            [Score.S] * 2 + [Score.N] * 3))
+        self.assertEqual(Score.N, compute_programming_score(
+            [Score.S] * 3 + [Score.U] * 2))
+        self.assertEqual(Score.N, compute_programming_score(
+            [Score.S] * 2 + [Score.U] * 3))
+
     def test_all_possible_U(self):
-        self.assertEqual(Score.U, compute_programming_score([Score.S] + [Score.N] * 4))
+        self.assertEqual(Score.U, compute_programming_score(
+            [Score.S] + [Score.N] * 4))
         self.assertEqual(Score.U, compute_programming_score([Score.N] * 5))
+        self.assertEqual(Score.U, compute_programming_score(
+            [Score.S] + [Score.U] * 4))
+        self.assertEqual(Score.U, compute_programming_score([Score.U] * 5))
+
 
 class TestAnalysisCalculation(unittest.TestCase):
     def test_lga_q_4(self):
         self.assertEqual(Score.S, compute_analysis_score(lga_q_4["analysis"]))
+
     def test_lga_q_5(self):
         self.assertEqual(Score.E, compute_analysis_score(lga_q_5["analysis"]))
+
     def test_many_E(self):
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.S, Score.S, Score.S]))
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.E, Score.S, Score.S]))
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.E, Score.E, Score.S]))
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.E, Score.E, Score.E]))
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.S, Score.S, Score.N]))
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.E, Score.S, Score.N]))
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.S, Score.S, Score.U]))
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.E, Score.S, Score.U]))
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.E, Score.E, Score.N]))
-        self.assertEqual(Score.E, compute_analysis_score([Score.E, Score.E, Score.E, Score.E, Score.U]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.S, Score.S, Score.S]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.S, Score.S]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.E, Score.S]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.E, Score.E]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.S, Score.S, Score.N]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.S, Score.N]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.S, Score.S, Score.U]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.S, Score.U]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.E, Score.N]))
+        self.assertEqual(Score.E, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.E, Score.U]))
+
     def test_many_S(self):
-        self.assertEqual(Score.S, compute_analysis_score([Score.E, Score.S, Score.S, Score.S, Score.S]))
-        self.assertEqual(Score.S, compute_analysis_score([Score.S, Score.S, Score.S, Score.S, Score.S]))
-        self.assertEqual(Score.S, compute_analysis_score([Score.E, Score.S, Score.S, Score.S, Score.N]))
-        self.assertEqual(Score.S, compute_analysis_score([Score.S, Score.S, Score.S, Score.S, Score.N]))
-        self.assertEqual(Score.S, compute_analysis_score([Score.E, Score.S, Score.S, Score.S, Score.U]))
-        self.assertEqual(Score.S, compute_analysis_score([Score.S, Score.S, Score.S, Score.S, Score.U]))
+        self.assertEqual(Score.S, compute_analysis_score(
+            [Score.E, Score.S, Score.S, Score.S, Score.S]))
+        self.assertEqual(Score.S, compute_analysis_score(
+            [Score.S, Score.S, Score.S, Score.S, Score.S]))
+        self.assertEqual(Score.S, compute_analysis_score(
+            [Score.E, Score.S, Score.S, Score.S, Score.N]))
+        self.assertEqual(Score.S, compute_analysis_score(
+            [Score.S, Score.S, Score.S, Score.S, Score.N]))
+        self.assertEqual(Score.S, compute_analysis_score(
+            [Score.E, Score.S, Score.S, Score.S, Score.U]))
+        self.assertEqual(Score.S, compute_analysis_score(
+            [Score.S, Score.S, Score.S, Score.S, Score.U]))
+
     def test_many_N(self):
-        self.assertEqual(Score.N, compute_analysis_score([Score.E, Score.N, Score.N, Score.N, Score.N]))
-        self.assertEqual(Score.N, compute_analysis_score([Score.S, Score.N, Score.N, Score.N, Score.N]))
-        self.assertEqual(Score.N, compute_analysis_score([Score.S, Score.S, Score.N, Score.N, Score.N]))
-        self.assertEqual(Score.N, compute_analysis_score([Score.S, Score.S, Score.S, Score.N, Score.N]))
-        self.assertEqual(Score.N, compute_analysis_score([Score.E, Score.S, Score.S, Score.N, Score.N]))
-        self.assertEqual(Score.N, compute_analysis_score([Score.E, Score.E, Score.S, Score.N, Score.N]))
-        self.assertEqual(Score.N, compute_analysis_score([Score.E, Score.E, Score.E, Score.N, Score.N]))
-        self.assertEqual(Score.N, compute_analysis_score([Score.E, Score.E, Score.E, Score.N, Score.U]))
-        self.assertEqual(Score.N, compute_analysis_score([Score.S, Score.S, Score.S, Score.N, Score.U]))
+        self.assertEqual(Score.N, compute_analysis_score(
+            [Score.E, Score.N, Score.N, Score.N, Score.N]))
+        self.assertEqual(Score.N, compute_analysis_score(
+            [Score.S, Score.N, Score.N, Score.N, Score.N]))
+        self.assertEqual(Score.N, compute_analysis_score(
+            [Score.S, Score.S, Score.N, Score.N, Score.N]))
+        self.assertEqual(Score.N, compute_analysis_score(
+            [Score.S, Score.S, Score.S, Score.N, Score.N]))
+        self.assertEqual(Score.N, compute_analysis_score(
+            [Score.E, Score.S, Score.S, Score.N, Score.N]))
+        self.assertEqual(Score.N, compute_analysis_score(
+            [Score.E, Score.E, Score.S, Score.N, Score.N]))
+        self.assertEqual(Score.N, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.N, Score.N]))
+        self.assertEqual(Score.N, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.N, Score.U]))
+        self.assertEqual(Score.N, compute_analysis_score(
+            [Score.S, Score.S, Score.S, Score.N, Score.U]))
+
     def test_many_U(self):
-        self.assertEqual(Score.U, compute_analysis_score([Score.U, Score.U, Score.U, Score.U, Score.U]))
-        self.assertEqual(Score.U, compute_analysis_score([Score.N, Score.U, Score.U, Score.U, Score.U]))
-        self.assertEqual(Score.U, compute_analysis_score([Score.N, Score.N, Score.U, Score.U, Score.U]))
-        self.assertEqual(Score.U, compute_analysis_score([Score.N, Score.N, Score.N, Score.U, Score.U]))
-        self.assertEqual(Score.U, compute_analysis_score([Score.E, Score.E, Score.E, Score.U, Score.U]))
-        self.assertEqual(Score.U, compute_analysis_score([Score.E, Score.S, Score.N, Score.U, Score.U]))
-        self.assertEqual(Score.U, compute_analysis_score([Score.S, Score.N, Score.N, Score.U, Score.U]))
+        self.assertEqual(Score.U, compute_analysis_score(
+            [Score.U, Score.U, Score.U, Score.U, Score.U]))
+        self.assertEqual(Score.U, compute_analysis_score(
+            [Score.N, Score.U, Score.U, Score.U, Score.U]))
+        self.assertEqual(Score.U, compute_analysis_score(
+            [Score.N, Score.N, Score.U, Score.U, Score.U]))
+        self.assertEqual(Score.U, compute_analysis_score(
+            [Score.N, Score.N, Score.N, Score.U, Score.U]))
+        self.assertEqual(Score.U, compute_analysis_score(
+            [Score.E, Score.E, Score.E, Score.U, Score.U]))
+        self.assertEqual(Score.U, compute_analysis_score(
+            [Score.E, Score.S, Score.N, Score.U, Score.U]))
+        self.assertEqual(Score.U, compute_analysis_score(
+            [Score.S, Score.N, Score.N, Score.U, Score.U]))
+
 
 class TestComprehensionCalculation(unittest.TestCase):
     def test_lga_q_4(self):
-        self.assertEqual(Score.S, compute_comprehension_score(lga_q_4["final"], lga_q_4["midterm"]))
+        self.assertEqual(Score.S, compute_comprehension_score(
+            lga_q_4["final"], lga_q_4["midterm"]))
+
     def test_lga_q_5(self):
-        self.assertEqual(Score.S, compute_comprehension_score(lga_q_5["final"], lga_q_5["midterm"]))
+        self.assertEqual(Score.S, compute_comprehension_score(
+            lga_q_5["final"], lga_q_5["midterm"]))
+
     def test_all_possible_E(self):
         # midterm doesn't matter
-        self.assertEqual(Score.E, compute_comprehension_score(Score.E, Score.E))
-        self.assertEqual(Score.E, compute_comprehension_score(Score.E, Score.S))
-        self.assertEqual(Score.E, compute_comprehension_score(Score.E, Score.N))
-        self.assertEqual(Score.E, compute_comprehension_score(Score.E, Score.U))
+        self.assertEqual(
+            Score.E, compute_comprehension_score(Score.E, Score.E))
+        self.assertEqual(
+            Score.E, compute_comprehension_score(Score.E, Score.S))
+        self.assertEqual(
+            Score.E, compute_comprehension_score(Score.E, Score.N))
+        self.assertEqual(
+            Score.E, compute_comprehension_score(Score.E, Score.U))
+
     def test_all_possible_S(self):
         # first case (final = S): midterm doesn't matter
-        self.assertEqual(Score.S, compute_comprehension_score(Score.S, Score.E))
-        self.assertEqual(Score.S, compute_comprehension_score(Score.S, Score.S))
-        self.assertEqual(Score.S, compute_comprehension_score(Score.S, Score.N))
-        self.assertEqual(Score.S, compute_comprehension_score(Score.S, Score.U))
+        self.assertEqual(
+            Score.S, compute_comprehension_score(Score.S, Score.E))
+        self.assertEqual(
+            Score.S, compute_comprehension_score(Score.S, Score.S))
+        self.assertEqual(
+            Score.S, compute_comprehension_score(Score.S, Score.N))
+        self.assertEqual(
+            Score.S, compute_comprehension_score(Score.S, Score.U))
         # second case (final = N, midterm = E)
-        self.assertEqual(Score.S, compute_comprehension_score(Score.N, Score.E))
+        self.assertEqual(
+            Score.S, compute_comprehension_score(Score.N, Score.E))
+
     def test_all_possible_N(self):
-        self.assertEqual(Score.N, compute_comprehension_score(Score.N, Score.S))
-        self.assertEqual(Score.N, compute_comprehension_score(Score.N, Score.N))
-        self.assertEqual(Score.N, compute_comprehension_score(Score.N, Score.U))
-        self.assertEqual(Score.N, compute_comprehension_score(Score.U, Score.E))
-        self.assertEqual(Score.N, compute_comprehension_score(Score.U, Score.S))
+        self.assertEqual(
+            Score.N, compute_comprehension_score(Score.N, Score.S))
+        self.assertEqual(
+            Score.N, compute_comprehension_score(Score.N, Score.N))
+        self.assertEqual(
+            Score.N, compute_comprehension_score(Score.N, Score.U))
+        self.assertEqual(
+            Score.N, compute_comprehension_score(Score.U, Score.E))
+        self.assertEqual(
+            Score.N, compute_comprehension_score(Score.U, Score.S))
+
     def test_all_possible_U(self):
-        self.assertEqual(Score.U, compute_comprehension_score(Score.U, Score.N))
-        self.assertEqual(Score.U, compute_comprehension_score(Score.U, Score.U))
-    
+        self.assertEqual(
+            Score.U, compute_comprehension_score(Score.U, Score.N))
+        self.assertEqual(
+            Score.U, compute_comprehension_score(Score.U, Score.U))
+
+
 class TestFinalGradeCalculation(unittest.TestCase):
     def test_lga_q_4(self):
         self.assertEqual('B', calculate_grade(
             lga_q_4["programming"], lga_q_4["analysis"], lga_q_4["midterm"], lga_q_4["final"]))
 
     def test_lga_q_5(self):
-        self.assertEqual('B', calculate_grade(lga_q_5["programming"], lga_q_5["analysis"], lga_q_5["midterm"], lga_q_5["final"]))
+        self.assertEqual('B', calculate_grade(
+            lga_q_5["programming"], lga_q_5["analysis"], lga_q_5["midterm"], lga_q_5["final"]))
 
     def test_all_possible_A(self):
         self.assertEqual('A', compute_final_score([Score.E, Score.E, Score.E]))
         self.assertEqual('A', compute_final_score([Score.E, Score.E, Score.S]))
-    
+
     def test_all_possible_B(self):
         self.assertEqual('B', compute_final_score([Score.E, Score.S, Score.S]))
-    
+
     def test_all_possible_C(self):
         self.assertEqual('C', compute_final_score([Score.S, Score.S, Score.S]))
 
@@ -129,6 +214,7 @@ class TestFinalGradeCalculation(unittest.TestCase):
         self.assertEqual('F', compute_final_score([Score.U, Score.S, Score.S]))
         self.assertEqual('F', compute_final_score([Score.U, Score.U, Score.S]))
         self.assertEqual('F', compute_final_score([Score.U, Score.U, Score.U]))
+
 
 if __name__ == '__main__':
     unittest.main()
